a79b895d83faeb400d85baae0bd5889bde1402ba
Luis Tejero <luisfaridta@gmail.com>
2018-06-19 15:29:37 +0100

    Card class added and tested

---
+++ specs/card_spec.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const Card = require('../card.js');
+
+describe('Card', function() {
+  let card;
+
+  beforeEach(function(){
+    card = new Card('Superman', 6, 9, 7);
+  })
+  it('should have a Name', function() {
+    const actualValue = card.name;
+    assert.strictEqual(actualValue, "Superman");
+
+  });
+
+
+  it('should have intelligence', function() {
+    const actualValue = card.intelligence;
+    assert.strictEqual(actualValue, 6);
+  });
+
+  it('should have strength', function() {
+    const actualValue = card.strength;
+    assert.strictEqual(actualValue, 9);
+  });
+
+  it('should have agility', function() {
+    const actualValue = card.agility;
+    assert.strictEqual(actualValue, 7);
+  });
+
+});
